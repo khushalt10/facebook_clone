d43be5df043a3510a5e47fd6a6fc764022de198e
unknown <khushalthepane2000@gmail.com>
2020-09-22 21:30:07 +0530

    header css

---
+++ src/Header.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import SearchIcon from '@material-ui/icons/Search';
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import FlagRoundedIcon from '@material-ui/icons/FlagRounded';
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
+import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import AddIcon from '@material-ui/icons/Add';
+import ForumRoundedIcon from '@material-ui/icons/ForumRounded';
+import NotificationsActiveRoundedIcon from '@material-ui/icons/NotificationsActiveRounded';
+import ExpandMoreRoundedIcon from '@material-ui/icons/ExpandMoreRounded';
+import { Avatar, IconButton } from '@material-ui/core';
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo%282019%29.svg.png"
+                    alt=""
+                />
+                <div className="header__input">
+                    <SearchIcon />
+                    <input rype="text" />
+                </div>
+            </div>
+
+            <div className="header__middle">
+                <div className="header__option">
+                    <HomeRoundedIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <FlagRoundedIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <SubscriptionsOutlinedIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <StorefrontOutlinedIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircleIcon fontSize="large" />
+                </div>
+                
+            </div>
+
+            <div className="header__right">
+                <div className="header__info">
+                    <Avatar />
+                    <h4>Khussshal</h4>
+                </div>
+
+                <IconButton>
+                    <AddIcon />
+                </IconButton>
+                <IconButton>
+                    <ForumRoundedIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsActiveRoundedIcon />
+                </IconButton>
+                <IconButton>
+                    <ExpandMoreRoundedIcon />
+                </IconButton>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDCFg0JXxmyeKi5sOlyRAK5d_FxkESCVrE",
+    authDomain: "fakebook-1de58.firebaseapp.com",
+    databaseURL: "https://fakebook-1de58.firebaseio.com",
+    projectId: "fakebook-1de58",
+    storageBucket: "fakebook-1de58.appspot.com",
+    messagingSenderId: "20977846947",
+    appId: "1:20977846947:web:0e70c658914b9715d64d16"
+  };
